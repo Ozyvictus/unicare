58d35515c98d90fb3289d6e3d470c612762819ae
Ozyvictus <82758221+Ozyvictus@users.noreply.github.com>
2025-03-10 13:45:17 +0530

    fixed migration errors

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,31 +1,43 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 
 class MedicalProfile(models.Model):
-    BLOOD_GROUP_CHOICES = {
-        "A+": "A+",
-        "A-": "A-",
-        "B+": "B+",
-        "B-": "B-",
-        "AB+": "AB+",
-        "AB-": "AB-",
-        "O+": "O+",
-        "O-": "O-"
-    }
-    GENDER_CHOICES = {
-        "M": "Male",
-        "F": "Female",
-        "O": "Other"
-    }
+    APLUS = "AP"
+    AMINUS = "AN"
+    BPLUS = "BP"
+    BMINUS = "BN"
+    ABPLUS = "ABP"
+    ABMINUS = "ABN"
+    OPLUS = "OP"
+    OMINUS = "ON"
+    MALE = "M"
+    FEMALE = "F"
+    OTHER = "O"
+    BLOOD_GROUP_CHOICES = [
+        (APLUS, "A+"),
+        (AMINUS, "A-"),
+        (BPLUS, "B+"),
+        (BMINUS, "B-"),
+        (ABPLUS, "AB+"),
+        (ABMINUS, "AB-"),
+        (OPLUS, "O+"),
+        (OMINUS, "O-")
+    ]
+    GENDER_CHOICES = [
+        (MALE, "Male"),
+        (FEMALE, "Female"),
+        (OTHER, "Other")
+    ]
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
-    bloodgroup = models.CharField(max_length=2, choices=BLOOD_GROUP_CHOICES)
+    bloodgroup = models.CharField(max_length=3, choices=BLOOD_GROUP_CHOICES)
     age = models.IntegerField()
     gender = models.CharField(choices=GENDER_CHOICES)
-    allergies = models.ArrayField(model.CharField(max_length=20, blank=True))
-    height = models.DecimalField()
-    weight = models.DecimalField()
+    allergies = ArrayField(models.CharField(max_length=20, blank=True))
+    height = models.DecimalField(decimal_places=2, max_digits=5)
+    weight = models.DecimalField(decimal_places=2, max_digits=5)
     patient_id = models.PositiveIntegerField()
 
     def add_visit(self):
