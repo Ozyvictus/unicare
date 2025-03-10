d7054ce21c823359f4de9f80afb192bd617b5d3a
Yasharth011 <yasharthgupta10@gmail.com>
2025-03-10 18:45:44 +0530

    Sidebar : added icons

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,41 +1,66 @@
 import React from "react";
 import '../style.css'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ReportIcon from '@mui/icons-material/Report';
+import HomeIcon from '@mui/icons-material/Home';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+const Sidebar_Data = [
+	{
+		title: "Home",
+		icon: <HomeIcon color="auction" />,
+		link: "/home",
+	},
+	{
+		title: "Accident Report ",
+		icon: <ReportIcon color="auction" />,
+		link: "/home",
+	},
+	{
+		title: "Profile",
+		icon: <AccountCircleIcon color="auction" />,
+		link: "/home",
+	},
+	{
+		title: "Appointment",
+		icon: <MedicalServicesIcon color="auction" />,
+		link: "/home",
+	},
+	{
+		title: "Mental Health",
+		icon: <PsychologyIcon color="auction" />,
+		link: "/home",
+	},
+];
 
 function Sidebar() {
-  return (
-    <aside className="sidebar">
-      <div className="logo">
-        <div className="logo-circle">
-          <div className="logo-icon"></div>
-        </div>
-      </div>
-      <nav className="sidebar-nav">
-        <ul>
-          <li className="nav-item active">
-            <span className="nav-icon dashboard"></span>
-          </li>
-          <li className="nav-item">
-            <span className="nav-icon users"></span>
-          </li>
-          <li className="nav-item">
-            <span className="nav-icon messages"></span>
-          </li>
-          <li className="nav-item">
-            <span className="nav-icon calendar"></span>
-          </li>
-          <li className="nav-item">
-            <span className="nav-icon documents"></span>
-          </li>
-          <li className="nav-item">
-            <span className="nav-icon reports"></span>
-          </li>
-          <li className="nav-item">
-            <span className="nav-icon folders"></span>
-          </li>
-        </ul>
-      </nav>
-    </aside>
-  );
-}
+	return (
+		<aside className="sidebar">
+			<div className="logo">
+				<div className="logo-circle">
+				</div>
+				</div>
+				<div className="sidebar-nav">
+					<ul>
+						{Sidebar_Data.map((val, key) => {
+							return (
+								<li
+									key={key}
+									className="item"
+									onCLick{...() => {
+										window.location.pathname = val.link
+									}}
+								>
+									<div>{val.icon}</div> </li>
+							);
+						})
+						}
+					</ul>
+
+			</div>
+		</aside>
+	);
+};
+
 
 export default Sidebar;
