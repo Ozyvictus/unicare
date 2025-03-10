02c45d02e1fc6099849ecd1ecc908b69e076ae65
Ozyvictus <aniketnandy784@gmail.com>
2025-03-10 15:52:01 +0530

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
-import Profile from './pages/Profile.jsx';
+import react from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import Profile from "./pages/Profile.jsx"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function Logout() {
+	localStorage.clear()
+	return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+	localStorage.clear()
+	return <Register />
+}
 
 function App() {
 	return (
-	<div> <Profile /> </div>
-	);
-};
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route
+					path="/home"
+					element={
+						<ProtectedRoute>
+							<Home />
+						</ProtectedRoute>
+					}
+				/>
+				<Route path="/logout" element={<Logout />} />
+				<Route path="/register" element={<RegisterAndLogout />} />
+				<Route path="*" element={<NotFound />}></Route>
+			</Routes>
+		</BrowserRouter>
+	)
+}
 
-export default App;
+export default App
