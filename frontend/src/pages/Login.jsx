983cbc30572f775dead8452663e7481c334c7cdc
Yasharth011 <yasharthgupta10@gmail.com>
2025-03-10 23:49:06 +0530

    Login Fixes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
+import AuthSection from "../components/AuthSection"
+import NoticesSection from "../components/NoticesSection";
 function Login() {
-    return <div>LOGIN TO UNICARE</div>
-}
+	return (<div className='login-section'>
+		<NoticesSection />
+		<AuthSection />
+		</div>)
+	}
 
-export default Login
+export default Login;
